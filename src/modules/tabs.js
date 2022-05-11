aa3cb7aeb3d2a0b541bccdb650c04d28446e4c14
studstone <nikolas2996@gmail.com>
2022-05-11 15:03:58 +0300

    add shutdown animation

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -2,24 +2,32 @@ const tabs = () => {
     const tabPanel = document.querySelector('.service-header');
     const tabs = document.querySelectorAll('.service-header-tab');
     const tabContent = document.querySelectorAll('.service-tab');
+    const widht = document.documentElement.clientWidth;
+    console.log(widht);
+
 
     tabPanel.addEventListener('click', e => {
         if (e.target.closest('.service-header-tab')) {
             tabs.forEach((tab, index) => {
                 const tabButton = e.target.closest('.service-header-tab');
-                if (tab === tabButton) {
+                if (tab === tabButton && widht > 768) {
                     let i = 0;
 
                     tab.classList.add('active');
                     tabContent[index].classList.remove('d-none');
                     tabContent[index].style.opacity = 0;
+
                     const interval = setInterval(() => {
                         i += 0.1;
+
                         tabContent[index].style.opacity = i;
                         if (tabContent[index].style.opacity >= 1) {
                             clearInterval(interval);
                         }
                     }, 40);
+                } else if (tab === tabButton && widht <= 768) {
+                    tab.classList.add('active');
+                    tabContent[index].classList.remove('d-none');
                 } else {
                     tab.classList.remove('active');
                     tabContent[index].classList.add('d-none');
