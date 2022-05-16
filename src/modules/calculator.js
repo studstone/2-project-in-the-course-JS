e63a1db39267dc2c28760a9300d95efab6264c1d
studstone <nikolas2996@gmail.com>
2022-05-16 18:06:18 +0300

    add const in calculator.js

--- src/modules/calculator.js
+++ src/modules/calculator.js
@@ -1,3 +1,16 @@
-export const calculator = () => {
+export const calculator = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block');
+    const calcType = calcBlock.querySelector('.calc-type');
+    const calcSquare = calcBlock.querySelector('.calc-square');
+    const calcCount = calcBlock.querySelector('.calc-count');
+    const calcDay = calcBlock.querySelector('.calc-day');
+    const spanTotal = document.getElementById('total');
 
+    const countCalc = () => {
+        console.log('calc');
+    };
+
+    calcBlock.addEventListener('change', e => {
+        countCalc();
+    });
 };
